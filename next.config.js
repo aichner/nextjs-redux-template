c1e6ea4b5a7014a918fd50410a1f50294cccbd81
Christian Aichner <contact@aichner-christian.com>
2021-02-27 10:03:12 +0100

    Improve NextJS config
    The configuration has been greatly improved.

--- next.config.js
+++ next.config.js
@@ -1,32 +1,11 @@
 //#region > Imports
-const withSass = require("@zeit/next-sass");
-const withCSS = require("@zeit/next-css");
 const withFonts = require("next-fonts");
 const withImages = require("next-images");
 const withPlugins = require("next-compose-plugins");
 //#endregion
 
 //#region > Exports
-module.exports = [
-  withCSS(
-    withSass({
-      webpack(config, options) {
-        config.module.rules.push({
-          test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-          use: {
-            loader: "url-loader",
-            options: {
-              limit: 100000,
-            },
-          },
-        });
-
-        return config;
-      },
-    })
-  ),
-  withPlugins([withFonts, withImages]),
-];
+module.exports = withPlugins([[withFonts], [withImages]]);
 //#endregion
 
 /**
